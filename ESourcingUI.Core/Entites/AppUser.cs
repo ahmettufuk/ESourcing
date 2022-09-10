ef3f95b9f81d303c4731042ae9779e1616b3943d
ahmettufuk <ahmetufukcoban@hotmail.com>
2022-09-10 17:03:17 +0300

    Mvc-Migrations

---
+++ ESourcingUI.Core/Entites/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ESourcingUI.Core.Entites
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsSeller { get; set; }
+        public bool IsBuyer { get; set; }
+
+    }
+}
---
+++ ESourcingUI.Infrastracture/DataAccess/WebAppContext.cs
@@ -0,0 +1,21 @@
+﻿using ESourcingUI.Core.Entites;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESourcingUI.Infrastracture.DataAccess
+{
+    public class WebAppContext : IdentityDbContext
+    {
+        public WebAppContext(DbContextOptions<WebAppContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
